932623a1c723df84401734c6068267af1579d86e
RohithAdmin <rohith.d@avasoft.com>
2024-05-22 20:02:52 +0530

    Create hello.js
    Testing for CAD

---
+++ hello.js
@@ -0,0 +1,14 @@
+// This is a simple "Hello, World!" script in ServiceNow.
+// You can customize this script according to your application logic.
+
+(function() {
+    'use strict';
+
+    // Define the main function
+    function helloWorld() {
+        gs.info('Hello, World! This is a sample script in ServiceNow.');
+    }
+
+    // Call the main function when the script is executed
+    helloWorld();
+})();
